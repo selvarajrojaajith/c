3f903d22a80dd914e66ffce3f7a1fbd036aaa232
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-05 15:58:37 +0530

    c program
    pgm

---
+++ positive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("\n Enter the positive number");
+scanf("%d",a);
+printf("\n Enter the negative number");
+scanf("%d",b);
+printf("\n enter the positive&negative");
+{
+getch();
+}
+}
